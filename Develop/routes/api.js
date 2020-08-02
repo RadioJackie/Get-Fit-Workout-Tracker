2df8906ce5d7423faa04d349f8afaf800fe6652a
RadioJackie <jacobpeters628@gmail.com>
2020-08-01 23:25:23 -0700

    finished with the routes api.js

---
+++ Develop/routes/api.js
@@ -0,0 +1,43 @@
+const router = require("express").Router();
+const db = require("../models");
+
+
+
+router.get("/workouts", (req, res) => {
+    db.Workout.find({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+  
+  
+  router.put('/workouts/:id', (req, res) => {
+    db.Workout.findByIdAndUpdate(req.params.id, {
+      $push: {
+        exercises: req.body
+      }
+    }).then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+  
+  
+  router.get("/workouts/range", (req, res) => {
+    db.Workout.find({})
+      .limit(7)
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+  
+  module.exports = router;
+  
